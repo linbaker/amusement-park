985cc64e8fe18681fdb3f776b8578128d0445ab8
AJ Ancheta and Lindsey Baker <aj@>
2019-03-28 14:10:36 -0700

    Fix quotation mark mismatch

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,19 @@ $(document).ready(function() {
   var height = parseInt(prompt("Enter your height (in inches):"));
 
   if(height <= 35){
-    $('div').removeClass("starlight");
+    $("div").removeClass("starlight");
     $("#rideOne").addClass("starlight");
 
   } else if (height <= 48){
-    $('div').removeClass("starlight");
-    $("#rideOne").addClass('starlight');
-    $("#rideTwo").addClass('starlight');
+    $("div").removeClass("starlight");
+    $("#rideOne").addClass("starlight");
+    $("#rideTwo").addClass("starlight");
 
   } else if (height >= 49){
-    $('div').removeClass("starlight");
-    $("#rideTwo").addClass('starlight');
-    $("#rideThree").addClass('starlight');
-    
+    $("div").removeClass("starlight");
+    $("#rideTwo").addClass("starlight");
+    $("#rideThree").addClass("starlight");
+
   } else {
     alert("Enter a valid height");
     parseInt(prompt("Enter your height (in inches)"));
