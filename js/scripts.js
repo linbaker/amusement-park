cd23625fc7cdf652cdf07777ad773c003c92c12f
AJ Ancheta and Lindsey Baker <aj@>
2019-03-28 14:07:42 -0700

    Remove show and hide function and replace with highlight

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,19 @@ $(document).ready(function() {
   var height = parseInt(prompt("Enter your height (in inches):"));
 
   if(height <= 35){
-    $('#rideOne').show();
+    $('div').removeClass("starlight");
+    $("#rideOne").addClass("starlight");
+
   } else if (height <= 48){
-    $("#rideOne").show();
-    $("#rideTwo").show();
-    $("#rideThree").hide();
+    $('div').removeClass("starlight");
+    $("#rideOne").addClass('starlight');
+    $("#rideTwo").addClass('starlight');
+
   } else if (height >= 49){
-    $("#rideOne").show();
-    $("#rideTwo").show();
-    $("#rideThree").show();
+    $('div').removeClass("starlight");
+    $("#rideTwo").addClass('starlight');
+    $("#rideThree").addClass('starlight');
+    
   } else {
     alert("Enter a valid height");
     parseInt(prompt("Enter your height (in inches)"));
